bc7dc2261694fd8feaf12faee04235f6f9616c0e
Nicolas Cordoba <126732707+Ckancord33@users.noreply.github.com>
2025-07-09 14:50:40 -0500

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,27 @@
+-- 1.  ¿Cuál es el nombre y la superficie terrestre de cada continente?
+SELECT NAME, AREA
+FROM CONTINENT;
+
+-- 2.  ¿Cuál es la superficie y la profundidad del lago Malawi? 
+SELECT NAME, AREA, DEPTH
+FROM LAKE
+WHERE NAME LIKE '%Malawi%';
+
+-- 3.  ¿Cuáles son los nombres y la superficie de todos los lagos en orden decreciente de profundidad? 
+SELECT NAME, AREA, DEPTH
+FROM LAKE
+ORDER BY DEPTH;
+
+-- 4.  ¿Cuál es el nombre de los países donde la agricultura representa al menos el 50% del producto interno bruto? -- use INNER JOIN ... ON 
+SELECT NAME
+FROM COUNTRY
+  INNER JOIN ECONOMY
+    ON COUNTRY.CODE = ECONOMY.COUNTRY
+WHERE ECONOMY.AGRICULTURE >= 50;
+
+-- 5.  ¿Cómo se llama el río más largo? -- use una subconsulta 
+SELECT NAME
+FROM RIVER
+WHERE LENGTH IS NOT NULL AND NOT EXISTS (
+  SELECT 1 FROM RIVER WHERE LENGTH > RIVER.LENGTH AND LENGTH IS NOT NULL
+);
